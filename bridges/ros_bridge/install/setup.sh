55276d9c6a2505c6c0505d683fb24b21166cea9e
DhlinV <hollis71025@gmail.com>
2025-02-17 04:53:03 -0800

    update ros2 api

--- bridges/ros_bridge/install/setup.sh
+++ bridges/ros_bridge/install/setup.sh
@@ -7,7 +7,7 @@
 # since a plain shell script can't determine its own path when being sourced
 # either use the provided COLCON_CURRENT_PREFIX
 # or fall back to the build time prefix (if it exists)
-_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/hollis/mnt/projects/MetaUrban-dev/bridges/ros_bridge/install
+_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/hollis/mnt/projects/metaurban/bridges/ros_bridge/install
 if [ ! -z "$COLCON_CURRENT_PREFIX" ]; then
   _colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX="$COLCON_CURRENT_PREFIX"
 elif [ ! -d "$_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX" ]; then
